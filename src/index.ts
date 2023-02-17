55f1afec84a508f964dc89549f2e53fdc8675f03
purry03 <rohansh.lko@gmail.com>
2023-02-17 11:31:20 +0530

    made annoucement board frontend

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import helmet from 'helmet';
+import minify from 'express-minify';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import { authExtract } from './routes/middlewares/auth';
@@ -18,7 +19,10 @@ app.set('view engine', 'ejs');  // set default view engine to ejs
 app.set('views', path.join(__dirname,'..', '/views'));   // set views directory
 
 // Basic Middlewares
-app.use(helmet());  // obscure headers for security
+helmet({
+	contentSecurityPolicy: false,
+});  // obscure headers for security
+app.use(minify());
 app.use('/static', express.static(path.join(__dirname,'..','/public'))); // set static directory
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
